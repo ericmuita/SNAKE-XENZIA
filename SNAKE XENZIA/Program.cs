78526e80e0e5f8c0904a7eeb806d67be966f008d
Eric <ericmuita25@gmail.com>
2018-07-07 15:09:05 +0300

    Add project files.

---
+++ SNAKE XENZIA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SNAKE_XENZIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
